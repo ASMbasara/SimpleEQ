880d6406a0f20b4c22a1047d379e24a45567a00a
ASMbasara <44873014+ASMbasara@users.noreply.github.com>
2023-11-25 12:53:45 +0000

    Set Up For Responding to Parameter Changes in the Editor

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -109,6 +109,19 @@ void SimpleEQAudioProcessorEditor::resized()
     bandQualitySlider.setBounds(bounds);
 }
 
+void SimpleEQAudioProcessorEditor::parameterValueChanged(int parameterIndex, float newValue)
+{
+    parametersChanged.set(true);
+}
+
+void SimpleEQAudioProcessorEditor::timerCallback()
+{
+    if (parametersChanged.compareAndSetBool(false, true)) {
+        //update monochain
+        //signal a repaint
+    }
+}
+
 std::vector<juce::Component*> SimpleEQAudioProcessorEditor::getComps()
 {
     
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -23,7 +23,7 @@ struct CustomRotarySlider : juce::Slider {
 //==============================================================================
 /**
 */
-class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor
+class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor, juce::AudioProcessorParameter::Listener, juce::Timer
 {
 public:
     SimpleEQAudioProcessorEditor (SimpleEQAudioProcessor&);
@@ -33,10 +33,47 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
 
+    /** Receives a callback when a parameter has been changed.
+
+            IMPORTANT NOTE: This will be called synchronously when a parameter changes, and
+            many audio processors will change their parameter during their audio callback.
+            This means that not only has your handler code got to be completely thread-safe,
+            but it's also got to be VERY fast, and avoid blocking. If you need to handle
+            this event on your message thread, use this callback to trigger an AsyncUpdater
+            or ChangeBroadcaster which you can respond to on the message thread.
+        */
+    void parameterValueChanged(int parameterIndex, float newValue) override;
+
+    /** Indicates that a parameter change gesture has started.
+
+        E.g. if the user is dragging a slider, this would be called with gestureIsStarting
+        being true when they first press the mouse button, and it will be called again with
+        gestureIsStarting being false when they release it.
+
+        IMPORTANT NOTE: This will be called synchronously, and many audio processors will
+        call it during their audio callback. This means that not only has your handler code
+        got to be completely thread-safe, but it's also got to be VERY fast, and avoid
+        blocking. If you need to handle this event on your message thread, use this callback
+        to trigger an AsyncUpdater or ChangeBroadcaster which you can respond to later on the
+        message thread.
+    */
+    void parameterGestureChanged(int parameterIndex, bool gestureIsStarting) override { }
+
+    //==============================================================================
+    /** The user-defined callback routine that actually gets called periodically.
+
+        It's perfectly ok to call startTimer() or stopTimer() from within this
+        callback to change the subsequent intervals.
+    */
+    void timerCallback() override;
+
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SimpleEQAudioProcessor& audioProcessor;
+    MonoChain monoChain;
+
+    juce::Atomic<bool> parametersChanged{ false };
 
     CustomRotarySlider bandFreqSlider, bandGainSlider, bandQualitySlider, lowCutFreqSlider, highCutFreqSlider, lowCutSlopeSlider, highCutSlopeSlider;
 
@@ -51,5 +88,29 @@ private:
 
     std::vector<juce::Component*> getComps();
 
+    template<int Index, typename Filter>
+    void getMagForFreqSingle(double &mag, Filter &filter, const double freq);
+    template<typename Filter>
+    void getMagForFreq(double& mag, Filter &filter, const int nFilters, const double freq);
+
+
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
 };
+
+template<int Index, typename Filter>
+inline void SimpleEQAudioProcessorEditor::getMagForFreqSingle(double &mag, Filter &filter, const double freq)
+{
+    if (!filter.isBypassed<Index>())
+        mag *= filter.get<Index>().coefficients->getMagnitudeForFrequency(freq, audioProcessor.getSampleRate());
+}
+
+template<typename Filter>
+inline void SimpleEQAudioProcessorEditor::getMagForFreq(double& mag, Filter &filter, const int nFilters, const double freq)
+{
+    
+    getMagForFreqSingle<0>(mag, filter, freq);
+    getMagForFreqSingle<1>(mag, filter, freq);
+    getMagForFreqSingle<2>(mag, filter, freq);
+    getMagForFreqSingle<3>(mag, filter, freq);
+}
